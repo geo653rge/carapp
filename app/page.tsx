cbe071b2b3f3c425fef39782f83f6f70d9393545
geo653rge <georgekimanikiringa@gmail.com>
2024-04-11 09:47:12 +0300

    added navbar,footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Hero } from "@/components";
+import { Hero,Navbar } from "@/components";
 export default function Home() {
   return (
     <main className="overflow-hidden">
 
       <Hero/>
-      {/* <Navbar/> */}
+      <Navbar/>
 
      </main>
   );
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,8 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
+import Footer from "./Footer";
 
 
 
-export{Hero, Navbar, CustomButton};
+export{Hero, Navbar, CustomButton, Footer};
